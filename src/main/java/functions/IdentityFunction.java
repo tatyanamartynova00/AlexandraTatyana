d9452a4ec698002df8aa0149880bae2f4cb6b112
Yudina <ladyalexyudina@gmail.com>
2019-09-14 22:19:39 +0300

    создание класса IdentityFunction

--- src/main/java/functions/IdentityFunction.java
+++ src/main/java/functions/IdentityFunction.java
@@ -1,6 +1,7 @@
 package functions;
 
 public class IdentityFunction implements MathFunction {
+    @Override
     public double apply(double x) {
         return x;
     }
--- src/main/java/functions/SqrFunction.java
+++ src/main/java/functions/SqrFunction.java
@@ -1,7 +1,8 @@
 package functions;
 
 public class SqrFunction implements MathFunction{
-public double apply(double x){
+    @Override
+    public double apply(double x){
     return Math.pow(x,2);
 }
 }
