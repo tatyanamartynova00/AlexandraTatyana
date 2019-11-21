3943dd8ae4fdd7686231dc5634916e063c11f3b1
Martynova <tatyanamartynova01@mail.ru>
2019-11-22 00:10:48 +0400

    создание

--- src/test/java/ru/ssau/tk/pion/alexandratatyana/operations/LeftSteppingDifferentialOperatorTest.java
+++ src/test/java/ru/ssau/tk/pion/alexandratatyana/operations/LeftSteppingDifferentialOperatorTest.java
@@ -10,7 +10,7 @@ public class LeftSteppingDifferentialOperatorTest {
     public void testDerive() {
         double step = 4;
         SteppingDifferentialOperator differentialOperator = new LeftSteppingDifferentialOperator(step);
-        assertEquals(differentialOperator.derive(new SqrFunction()).apply(9), (Math.pow(9,2) - Math.pow((9 - step),3)) / step);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(9), (Math.pow(9,2) - Math.pow((9 - step),2)) / step);
         assertEquals(differentialOperator.derive(new CubeFunction()).apply(2), (Math.pow(2,3) - Math.pow((2 - step),3))/ step);
     }
 }
