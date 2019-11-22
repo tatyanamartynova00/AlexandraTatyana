d0e7c1dff31c1f995163f3e9bd19ed6cefce31d2
Yudina <ladyalexyudina@gmail.com>
2019-11-22 13:00:25 +0300

    create TubDiffOpTest

---
+++ src/test/java/ru/ssau/tk/pion/alexandratatyana/operations/TabulatedDifferentialOperatorTest.java
@@ -0,0 +1,39 @@
+package ru.ssau.tk.pion.alexandratatyana.operations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ru.ssau.tk.pion.alexandratatyana.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.pion.alexandratatyana.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.pion.alexandratatyana.functions.factory.LinkedListTabulatedFunctionFactory;
+
+class TabulatedDifferentialOperatorTest {
+    TabulatedDifferentialOperator array = new TabulatedDifferentialOperator(new ArrayTabulatedFunctionFactory());
+    TabulatedDifferentialOperator isArray = new TabulatedDifferentialOperator();
+    TabulatedDifferentialOperator linkedList = new TabulatedDifferentialOperator(new LinkedListTabulatedFunctionFactory());
+    double[] xValues = {5.3, 7.45, 15.};
+    double[] yValues = {8.2, 9.85, 20.};
+    ArrayTabulatedFunction myFunc = new ArrayTabulatedFunction(xValues, yValues);
+
+    @Test
+    public void testDerive() {
+        var diffArray = array.derive(myFunc);
+        assertEquals(diffArray.getY(0), (9.85 - 8.2) / (7.45 - 5.3), 0.0001);
+        assertEquals(diffArray.getY(1), (20 - 9.85) / (15 - 7.45), 0.0001);
+        assertEquals(diffArray.getY(2), (20 - 9.85) / (15 - 7.45), 0.0001);
+    }
+
+    @Test
+    public void testGetFactory() {
+        assertTrue(isArray.getFactory() instanceof ArrayTabulatedFunctionFactory);
+        assertTrue(linkedList.getFactory() instanceof LinkedListTabulatedFunctionFactory);
+    }
+
+    @Test
+    public void testSetFactory() {
+        isArray.setFactory(new LinkedListTabulatedFunctionFactory());
+        assertTrue(isArray.getFactory() instanceof LinkedListTabulatedFunctionFactory);
+    }
+
+}
