a7aea3d7bf8557e8d0575c784708d11ad791c1fb
Yudina <ladyalexyudina@gmail.com>
2019-12-08 16:24:52 +0300

    добавлен тест метода deriveSynchronously

--- src/test/java/ru/ssau/tk/pion/alexandratatyana/operations/TabulatedDifferentialOperatorTest.java
+++ src/test/java/ru/ssau/tk/pion/alexandratatyana/operations/TabulatedDifferentialOperatorTest.java
@@ -1,12 +1,12 @@
 package ru.ssau.tk.pion.alexandratatyana.operations;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 import ru.ssau.tk.pion.alexandratatyana.functions.ArrayTabulatedFunction;
 import ru.ssau.tk.pion.alexandratatyana.functions.factory.ArrayTabulatedFunctionFactory;
 import ru.ssau.tk.pion.alexandratatyana.functions.factory.LinkedListTabulatedFunctionFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TabulatedDifferentialOperatorTest {
     TabulatedDifferentialOperator array = new TabulatedDifferentialOperator(new ArrayTabulatedFunctionFactory());
@@ -36,4 +36,11 @@ class TabulatedDifferentialOperatorTest {
         assertTrue(isArray.getFactory() instanceof LinkedListTabulatedFunctionFactory);
     }
 
+    @Test
+    public void testDeriveSynchronously() {
+        var diffArray = array.deriveSynchronously(myFunc);
+        assertEquals(diffArray.getY(0), (9.85 - 8.2) / (7.45 - 5.3), 0.0001);
+        assertEquals(diffArray.getY(1), (20. - 9.85) / (15. - 7.45), 0.0001);
+        assertEquals(diffArray.getY(2), (20. - 9.85) / (15. - 7.45), 0.0001);
+    }
 }
